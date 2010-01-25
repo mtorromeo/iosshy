c6a129b440e5a8e668b92eb154b8406881f0ac13
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2010-01-25 23:36:12 +0100

    Added setuptools script

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+py2app/py2exe build script for IOSSHy
+
+Will automatically ensure that all build prerequisites are available
+via ez_setup
+
+Usage (Mac OS X):
+    python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
+"""
+import sys, os
+from setuptools import setup, find_packages
+
+mainscript = 'bin/iosshy'
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+from PyQt4 import uic
+for form in ("tunneldialog.ui",):
+	uif = "Ui_{0}.py".format( form.rpartition('.')[0] )
+	with open(os.path.join("iosshy", uif), "wb") as f:
+		uic.compileUi(os.path.join("iosshy", form), f)
+
+if sys.platform == 'darwin':
+	extra_options = dict(
+		setup_requires=['py2app'],
+		app=[mainscript],
+		options={"py2app": {
+			"argv_emulation": True,
+			"includes": ["sip", "PyQt4.QtCore", "PyQt4.QtGui"]
+		}},
+	)
+elif sys.platform == 'win32':
+	import py2exe
+	
+	origIsSystemDLL = py2exe.build_exe.isSystemDLL
+	def isSystemDLL(pathname):
+		if os.path.basename(pathname).lower() == "msvcp90.dll":
+			return 0
+		return origIsSystemDLL(pathname)
+	py2exe.build_exe.isSystemDLL = isSystemDLL
+	
+	extra_options = dict(
+		windows=[{
+			"script": mainscript,
+			#"icon_resources": [(0, "app.ico")]
+		}],
+		options={"py2exe": {
+			"optimize": 2,
+			"skip_archive": True,
+			"includes": ["sip"]
+		}}
+	)
+else:
+	extra_options = dict(
+		scripts=[mainscript],
+		packages = find_packages()
+	)
+
+setup(
+	name="IOSSHy",
+	version="1.0",
+	description='Desktop tool to quickly setup SSH tunnels and automatically execute commands that make use of them',
+	long_description=open(README).read(),
+	keywords='qt ssh tunnel',
+	author="Massimiliano Torromeo",
+	author_email="massimiliano.torromeo@gmail.com",
+	url="http://github.com/mtorromeo/iosshy",
+	license="MIT License",
+	**extra_options
+)
